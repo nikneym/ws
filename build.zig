8c35142f7bea96c594be3abd6f8b1bded6b71a89
nikneym <halildrk@gmail.com>
2023-02-09 03:00:23 +0300

    build.zig: Remove pointless comments

--- build.zig
+++ build.zig
@@ -1,16 +1,6 @@
 const std = @import("std");
-//const deps = @import("deps.zig");
 
 pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
-    //const target = b.standardTargetOptions(.{});
-
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
-    //const mode = b.standardReleaseOptions();
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
